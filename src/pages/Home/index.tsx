6bb61de1331641241ac3b5f46333fd3669155d64
MUedaM <mi-ueda@outlook.com>
2026-02-13 18:50:30 -0300

    Finalizando parte 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,14 +2,12 @@ import ListHome from '../../containers/ListHome'
 import Footer from '../../components/Footer'
 import HeaderHome from '../../components/HeaderHome'
 
-const Home = () => {
-  return (
-    <>
-      <HeaderHome />
-      <ListHome />
-      <Footer />
-    </>
-  )
-}
+const Home = () => (
+  ;<>
+    <HeaderHome />
+    <ListHome />
+    <Footer />
+  </>
+)
 
 export default Home
